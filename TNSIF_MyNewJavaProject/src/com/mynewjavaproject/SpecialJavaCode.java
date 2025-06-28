47d1c547e967227573bb7adfe5f94d776e6f39cb
Asus-2025 <Asus-2025@LAPTOP-0TP7JPV2>
2025-06-28 21:24:27 +0530

    its my first commit

---
+++ TNSIF_MyNewJavaProject/src/com/mynewjavaproject/SpecialJavaCode.java
@@ -0,0 +1,26 @@
+package com.mynewjavaproject;
+
+public class SpecialJavaCode {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	int age = 25;
+	double salary = 40000.00;
+	boolean ispassed = false;
+	char grade = 'B';
+		
+	double currentage = age;
+	int currentsalary = (int) salary;
+	
+	int increment = 5000;
+	int total = increment + currentsalary;
+	
+	System.out.println(age);
+	System.out.println(salary);
+	System.out.println(currentsalary);
+	System.out.println(total);
+	
+	}
+
+}
