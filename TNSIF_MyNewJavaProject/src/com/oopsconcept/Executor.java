b5d58100d4812e06b96d4e831fc9d8a81ae56279
Asus-2025 <Asus-2025@LAPTOP-0TP7JPV2>
2025-06-30 21:34:29 +0530

    initial commit

---
+++ TNSIF_MyNewJavaProject/src/com/oopsconcept/Executor.java
@@ -0,0 +1,16 @@
+package com.oopsconcept;
+
+public class Executor {
+
+	public static void main(String[] args) {
+		
+    	EncapsulationDemo ed = new EncapsulationDemo();
+		ed.setName("Rohit Sharma");
+		ed.setAge(37);
+		ed.setJersyNo(45);
+		
+		System.out.println(ed);
+		
+	}
+
+}
