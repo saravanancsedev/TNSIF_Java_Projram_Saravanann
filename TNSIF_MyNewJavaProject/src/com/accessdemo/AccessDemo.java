47e108919096dffb66b3740d240874610a83c8c3
Asus-2025 <Asus-2025@LAPTOP-0TP7JPV2>
2025-07-01 21:50:12 +0530

    second 2

---
+++ TNSIF_MyNewJavaProject/src/com/accessdemo/AccessDemo.java
@@ -0,0 +1,23 @@
+package com.accessdemo;
+
+import com.packagesandaccessmodifiers.AccessModifiersExample;
+
+public class AccessDemo {
+
+	public static void main(String[] args) {
+
+	AccessModifiersExample ame = new AccessModifiersExample();
+		
+		System.out.println("Public Variable: "+ ame.publicVar);
+		/*
+		 * System.out.println("Private Variable: "+ ame.privateVar);
+		 * System.out.println("Protected Variable: "+ ame.protectedVar);
+		 * System.out.println("Default Variable: "+ ame.defaultVar);
+		 */
+
+
+	
+	}
+
+	}
+
---
+++ TNSIF_MyNewJavaProject/src/com/accessdemo/PackageMain.java
@@ -0,0 +1,14 @@
+package com.accessdemo;
+
+import com.packagesandaccessmodifiers.PackageExample;
+
+public class PackageMain {
+
+	public static void main(String[] args) {
+
+		PackageExample pe = new PackageExample();
+		pe.display();
+
+	}
+
+}
