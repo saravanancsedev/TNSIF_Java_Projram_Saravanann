12c0fdb8f7ab97bd79929ef38b244e7b214b3619
Asus-2025 <Asus-2025@LAPTOP-0TP7JPV2>
2025-07-12 13:04:33 +0530

    new commit

---
+++ TNSIF_MyNewJavaProject/src/com/tns/junit/testing/DisbaledDemo.java
@@ -0,0 +1,33 @@
+package com.tns.junit.testing;
+
+public class DisbaledDemo {
+	
+	
+	
+	@Test
+	 void test1()
+	{
+	System.out.println("Test 1");
+		
+		
+	}
+	
+	@Disabled
+	
+	@Test
+	 void test2()
+	{
+	System.out.println("Test 2");
+		
+		
+	}
+	
+	@Test
+	 void test3()
+	{
+	System.out.println("Test 3");
+		
+		
+	}
+
+}
---
+++ TNSIF_MyNewJavaProject/src/com/tnsif/lambdaexpression/MyCube.java
@@ -0,0 +1,8 @@
+package com.tnsif.lambdaexpression;
+
+@FunctionalInterface
+public interface MyCube {
+	int getCube(int no);
+	//void show(); not allow more than one abstract method
+}
+
